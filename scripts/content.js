a6bab944a0bcb84e82b4a3cf77c8e1827185f0d4
Udayan Maurya <edmauryaishu@gmail.com>
2023-02-05 13:00:03 -0500

    find matches

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,7 @@
-console.log("[Content]");
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+  const article = document.querySelector("article");
+  const re = new RegExp(request.message, "g");
+  const matches = article.textContent.matchAll(re);
+  console.log([...matches]);
+  sendResponse({ message: `response from content script ${request.message}` });
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,15 @@
 const searchInput = document.getElementById("search");
 
 searchInput.addEventListener("change", (e) => {
-  console.log("[Popup]", e.target.value);
+  (async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+    });
+    console.log(e.target.value);
+    const response = await chrome.tabs.sendMessage(tab.id, {
+      message: e.target.value,
+    });
+    // do something with response here, not outside the function
+    console.log("[Popup]", response);
+  })();
 });
